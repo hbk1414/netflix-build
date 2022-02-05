7fb710adb4a2ade24b42f8e5d32b4d2717612552
Harres Khan <hbk_khan@hotmail.co.uk>
2022-02-05 17:15:59 +0000

    ADD : added useState,UseEffect and animation nav

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDK--NVh3PSRtCPFNF1dror1fJAzUuQOfs",
+    authDomain: "netflix-clone-787d1.firebaseapp.com",
+    projectId: "netflix-clone-787d1",
+    storageBucket: "netflix-clone-787d1.appspot.com",
+    messagingSenderId: "279050939195",
+    appId: "1:279050939195:web:3983fa35c2a6f5840f251e"
+};
