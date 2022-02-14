2ffa4056e2995b174be6a6733647c3cbbf2c93f4
Harres Khan <hbk_khan@hotmail.co.uk>
2022-02-14 09:44:05 +0000

    trying to sort out row.js error

--- src/Banner.js
+++ src/Banner.js
@@ -9,7 +9,7 @@ function Banner() {
 
     useEffect(() => {
         async function fetchData() {
-            const request = await axios.get(requests.fetchNetflixOriginal)
+            const request = await axios.get(requests.fetchTrending)
             setMovie(
                 request.data.results[
                 Math.floor(Math.random() * request.data.results.length - 1)
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Nav from './Nav';
 import './HomeScreen.css'
 import Banner from './Banner';
+import requests from './Requests';
+import Row from './Row';
 
 function HomeScreen() {
     return (
@@ -11,7 +13,19 @@ function HomeScreen() {
 
             <Banner />
 
-            <Row title='Netflix Originals' />
+             <Row title='Netflix Originals' fetchURL={requests.fetchNetflixOriginal} isLargeRow /> 
+
+            <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+
+            <Row title='Action Movies' fetchURL={requests.fetchActionMovies} />
+
+            <Row title='Comedy Movies' fetchURL={requests.fetchComedyMovies} />
+
+            <Row title='Horror Movies' fetchURL={requests.fetchHorrorMovies} />
+
+            <Row title='Romance Movies' fetchURL={requests.fetchRomanceMovies} />
+
+            <Row title='Documentaries' fetchURL={requests.fetchDocumentaries} /> 
         </div>
     )
 
