266ac2a3b61c0db1eb721262fb5399049e40c8c5
Harres Khan <hbk_khan@hotmail.co.uk>
2022-02-16 00:39:35 +0000

    all the rows are now working and have been styled

--- src/Row.js
+++ src/Row.js
@@ -6,6 +6,8 @@ function Row({ title, fetchURL, isLargeRow = false }) {
 
     const [movies, setMovies] = useState([])
 
+    const base_url = "https://image.tmdb.org/t/p/original/"
+
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchURL)
@@ -19,6 +21,18 @@ function Row({ title, fetchURL, isLargeRow = false }) {
     return (
         <div className="row">
             <h2>{title}</h2>
+
+            <div className="row__posters">
+                {movies.map(
+                    (movie) =>
+                        ((isLargeRow && movie.poster_path) ||
+                            (!isLargeRow && movie.backdrop_path)) && (
+                            <img className={`row__poster ${isLargeRow && "row__posterLarge"}`} key={movie.id} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
+                                }`} alt="movie.name" />
+                        )
+
+                )}
+            </div>
         </div>
     )
 }
