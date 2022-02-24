a84ececd70168a1244148f5f5af677a8a44ac701
Harres Khan <hbk_khan@hotmail.co.uk>
2022-02-24 16:46:05 +0000

    adding the sign up screen and login screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+
+  const user = null
+
+  //{name : "john"}
+
   return (
     <div className="app">
 
       <Router>
-        <Switch>
-          <Route path="/">
-            <HomeScreen />
-          </Route>
-        </Switch>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+
       </Router>
     </div>
   );
