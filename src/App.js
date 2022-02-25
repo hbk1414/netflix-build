bc7400f8c1aaa1322da3604d0786c0d0f4022d3c
Harres Khan <hbk_khan@hotmail.co.uk>
2022-02-25 20:45:28 +0000

    err: cannot read user

--- src/App.js
+++ src/App.js
@@ -9,20 +9,29 @@ import {
 import LoginScreen from './screens/LoginScreen';
 import { useEffect } from 'react';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from "react-redux"
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
 
-  const user = null
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         //logged in
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email
+          })
+        )
       } else {
         //logged out
+        dispatch(logout)
       }
-    })
+    });
 
     return unsubscribe;
   }, [])
